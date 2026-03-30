19d7fde2003d641611c77b7e3344d85ec527eb30
jymeng18 <aizeuyn@gmail.com>
2026-03-29 21:05:13 -0700

    styles: react leaflet map

---
+++ app/src/components/ReportMap.tsx
@@ -0,0 +1,62 @@
+/**
+ * Leaflet Map component
+ */
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { type AnimalReport } from "../types";
+import { ReportStatus } from "../types";
+import { Link } from "react-router-dom";
+
+interface ReportMapProps {
+  reports: AnimalReport[];
+}
+
+function ReportMap({ reports }: ReportMapProps) {
+  // Only show animals with lost status
+  const lostReports = reports.filter((r) => r.status === ReportStatus.Lost);
+
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={12}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {lostReports.map((report) => {
+        if (!report.location) {
+          return null;
+        }
+
+        // For each valid report, assign a location marker to them on the map
+        return (
+          <Marker
+            position={[report.location.lat, report.location.lon]}
+            key={report.id}
+          >
+            <Popup>
+              <div style={{ textAlign: "center" }}>
+                <img
+                  src={report.animalPhotoURL}
+                  alt={report.animalName}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    objectFit: "cover",
+                  }}
+                />
+                <h6>{report.animalName}</h6>
+                <p>Type: {report.animalType}</p>
+                <Link to={`/report/${report.id}`}>View Details</Link>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
+}
+
+export default ReportMap;
---
+++ app/src/services/index.ts
@@ -0,0 +1,4 @@
+export { hashPassword, verifyPassword } from "./crypto";
+export { uploadImage } from "./imgbb"
+export { getReports, saveReport, updateReport } from "./jsonbin"
+export { reverseGeocode } from "./nominatim"
