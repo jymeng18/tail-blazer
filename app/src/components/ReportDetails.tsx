a1e6e1ca849693603ef8da47ec3230eebd1a8498
jymeng18 <aizeuyn@gmail.com>
2026-03-30 23:39:32 -0700

    fix: inconsitent loading/error hadnling states

--- app/src/components/ReportDetails.tsx
+++ app/src/components/ReportDetails.tsx
@@ -20,36 +20,23 @@ function ReportDetails() {
   const [passwordError, setPasswordError] = useState<string | null>(null);
   const [updating, setUpdating] = useState(false);
 
-  if (loading) {
-    return (
-      <div className="report-details-container">
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  // TODO: Use a Bootstrap alert instead.
-  if (error) {
-    return (
-      <div className="report-details-container">
-        <div className="error-message">{error}</div>
-      </div>
-    );
-  }
 
   // Find the specific report
   const report = reports.find((r) => r.id === id);
 
-  if (!report) {
+  if (error) {
     return (
       <div className="report-details-container">
-        <div className="not-found-message">Report not found.</div>
-        <button className="btn-back" onClick={() => navigate("/")}>
-          Back to Home
-        </button>
+        <section className="d-flex flex-column justify-content-center align-items-center">
+          <div className="not-found-message">Report not found.</div>
+          <button className="btn-back" onClick={() => navigate("/")}>
+            Back to Home
+          </button>
+        </section>
       </div>
     );
   }
+
 
   async function handleMarkAsFound() {
     if (!password) {
@@ -89,119 +76,143 @@ function ReportDetails() {
     }
   }
 
+  if (loading) {
+    return (
+      <div className="container text-center mt-5">
+        <div className="spinner-border text-primary" role="status"></div>
+        <div>
+          <span className="load-msgs">Loading Reports...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (!report) {
+    return (
+      <div className="report-details-container">
+        <section className="d-flex flex-column justify-content-center align-items-center">
+          <div className="not-found-message">Report not found.</div>
+          <button className="btn-back" onClick={() => navigate("/")}>
+            Back to Home
+          </button>
+        </section>
+      </div>
+    );
+  }
+
   return (
-    <div className="report-details-container">
-      <h2>{report.animalName}</h2>
+    <div>
+      <div className="report-details-container">
+        <h2>{report.animalName}</h2>
 
-      {report.animalPhotoURL && (
-        <img
-          src={report.animalPhotoURL}
-          alt={report.animalName}
-          className="report-image"
-        />
-      )}
+        {report.animalPhotoURL && (
+          <img
+            src={report.animalPhotoURL}
+            alt={report.animalName}
+            className="report-image"
+          />
+        )}
 
-      {/* Animal type */}
-      <div className="detail-field">
-        <strong>Type:</strong>
-        <p>{report.animalType}</p>
-      </div>
-      
-      {/* Pet status, found or lost */}
-      <div className="detail-field">
-        <strong>Status:</strong>
-        <p>
-          <span
-            className={`status-badge ${report.status === ReportStatus.Lost ? "lost" : "found"}`}
-          >
-            {report.status}
-          </span>
-        </p>
-      </div>
-
-      {/* Pet desc */}
-      <div className="detail-field">
-        <strong>Description:</strong>
-        <p>{report.desc}</p>
-      </div>
-
-      {/* owner contacts */}
-      <div className="detail-field">
-        <strong>Contact Information:</strong>
-        <p>{report.contactInfo}</p>
-      </div>
-
-
-      {/* Report location may be null if user marks middle of nowhere on map */}
-      {report.location && (
+        {/* Animal type */}
         <div className="detail-field">
-          <strong>Last Seen Location:</strong>
-          <p>{report.location.address}</p>
+          <strong>Type:</strong>
+          <p>{report.animalType}</p>
         </div>
-      )}
 
-      {/* date of post */}
-      <div className="detail-field">
-        <strong>Date Posted:</strong>
-        <p>{new Date(report.createdAt).toLocaleDateString()}</p>
-      </div>
+        {/* Pet status, found or lost */}
+        <div className="detail-field">
+          <strong>Status:</strong>
+          <p>
+            <span
+              className={`status-badge ${report.status === ReportStatus.Lost ? "lost" : "found"}`}
+            >
+              {report.status}
+            </span>
+          </p>
+        </div>
 
-      {report.status === ReportStatus.Lost && !showPasswordPrompt && (
-        <button
-          className="btn-mark-found"
-          onClick={() => setShowPasswordPrompt(true)}
-        >
-          Mark as Found
-        </button>
-      )}
+        {/* Pet desc */}
+        <div className="detail-field">
+          <strong>Description:</strong>
+          <p>{report.desc}</p>
+        </div>
 
-      
-      {/* User has attempted to mark as found */}
-      {showPasswordPrompt && (
-        <div className="password-prompt-container">
-          <h5>Enter Password to Mark as Found</h5>
-          {passwordError && (
-            <div className="password-error">{passwordError}</div>
+        {/* owner contacts */}
+        <div className="detail-field">
+          <strong>Contact Information:</strong>
+          <p>{report.contactInfo}</p>
+        </div>
+
+        {/* Report location may be null if user marks middle of nowhere on map */}
+        {report.location && (
+          <div className="detail-field">
+            <strong>Last Seen Location:</strong>
+            <p>{report.location.address}</p>
+          </div>
+        )}
+
+        {/* date of post */}
+        <div className="detail-field">
+          <strong>Date Posted:</strong>
+          <p>{new Date(report.createdAt).toLocaleDateString()}</p>
+        </div>
+
+        <div className="btn-mark-found-container">
+          {report.status === ReportStatus.Lost && !showPasswordPrompt && (
+            <button
+              className="btn-mark-found"
+              onClick={() => setShowPasswordPrompt(true)}
+            >
+              Mark as Found
+            </button>
           )}
-          <div className="password-input-wrapper">
-            <input
-              type="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-
-          <div className="button-group">
-            <button
-              className="btn-confirm"
-              onClick={handleMarkAsFound}
-              disabled={updating}
-            >
-              {updating ? "Updating..." : "Confirm"}
-            </button>
-
-            <button
-              className="btn-cancel"
-              onClick={() => {
-                setShowPasswordPrompt(false);
-                setPassword("");
-                setPasswordError(null);
-              }}
-            >
-              Cancel
-            </button>
-          </div>
         </div>
-      )}
 
-      <div className="back-button-container">
-        <button
-          className="btn-back"
-          onClick={() => navigate("/")}
-        >
-          Back to Home
-        </button>
+        <section></section>
+        {/* User has attempted to mark as found */}
+        {showPasswordPrompt && (
+          <div className="password-prompt-container">
+            <h5>Enter Password to Mark as Found</h5>
+            {passwordError && (
+              <div className="password-error">{passwordError}</div>
+            )}
+            <div className="password-input-wrapper">
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="button-group">
+              <button
+                className="btn-confirm"
+                onClick={handleMarkAsFound}
+                disabled={updating}
+              >
+                {updating ? "Updating..." : "Confirm"}
+              </button>
+
+              <button
+                className="btn-cancel"
+                onClick={() => {
+                  setShowPasswordPrompt(false);
+                  setPassword("");
+                  setPasswordError(null);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
+
+        <div className="back-button-container">
+          <button className="btn-back" onClick={() => navigate("/")}>
+            Back to Home
+          </button>
+        </div>
       </div>
     </div>
   );
